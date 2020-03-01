664537f51557c0fa0707c289131cdf2dc31e76d5
fdsn2014 <690449401@qq.com>
2020-03-01 11:54:03 +0800

    swap alg

--- test.c
+++ test.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include <time.h>
+#include <sys/time.h>
 //打印数组每行10个
-void print_str(int str[], int len){
+void print_arr(int arr[], int len){
     int i = 0;
     int j = 0;
     for(;i < len;i=i+10){
@@ -13,14 +13,14 @@ void print_str(int str[], int len){
             printf("%6d<->%6d:",i,i+9);    
         }
         for(j=i;(j<(i+10))&&(j<len);j++)
-            printf("%6d",str[j]);
+            printf("%6d",arr[j]);
         printf("\n");         
     } 
 }
-void initstr(int* str,int len){
+void initarr(int* arr,int len){
     int i = 0;
     while(i < len){
-        str[i] = rand()%1000;
+        arr[i] = rand()%10000;
         i++;    
     } 
     return;   
@@ -28,12 +28,12 @@ void initstr(int* str,int len){
 /*
 函数名：bubblesort
 功能：使用冒泡法对长度为len的无序数组排序
-入参：  str 未排序的数组
+入参：  arr 未排序的数组
         len 数组长度
-出参：  str 有序数组
+出参：  arr 有序数组
 返回值：无
 */
-void bubblesort(int *str,int len){
+void bubblesort(int *arr,int len){
     int i = 0 ,j =0;
     int temp = 0;
     int flag =0;//是否交互顺序
@@ -42,11 +42,11 @@ void bubblesort(int *str,int len){
         flag = 0;
         //未排序好部分两两比较
         for(j=0;j<len-i;j++){
-            if(str[j] > str[j+1])
+            if(arr[j] > arr[j+1])
             {
-                temp = str[j+1];
-                str[j+1] = str[j];
-                str[j] = temp;
+                temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
                 flag = 1;
             }    
         }
@@ -58,84 +58,111 @@ void bubblesort(int *str,int len){
 /*
 函数名：partion
 功能：使用快速法中分区
-入参：  str 未排序的数组
+入参：  arr 未排序的数组
         left 数组左下标
         riht 数组右下表
-出参：  str 有序数组
+出参：  arr 有序数组
 返回值：无
 */
-int partion(int *str, int left, int right){
-   int temp,pivot;//pivot存放主元
-   int i,j;
-   i = left;
-   pivot = str[right];
-   for(j = left;j < right;j++)
-   {
-       if(str[j] < pivot)
-       {  //交换值
-           temp = str[i];
-           str[i] = str[j];
-           str[j] = temp;
-           i++;
-       }
-   }
-   str[right] = str[i];
-   str[i] = pivot;
-   return i;//把主元的下标返回
+int partion(int *arr, int left, int right){
+ int pivot = arr[left];
+ int i = left + 1;
+ int j = right;
+ while(1)
+ {  
+   //向右遍历扫描
+   while(i <= j && arr[i] <= pivot) i++;
+   //向左遍历扫描
+   while(i <= j && arr[j] >= pivot) j--;
+   if(i >= j)
+     break;
+   //交换
+   int temp = arr[i];
+   arr[i] = arr[j];
+   arr[j] = temp;
+ }
+ //把arr[j]和主元交换
+ arr[left] = arr[j];
+ arr[j] = pivot;
+ return j;
 }
 /*
 函数名：quicksort
 功能：使用快速法对无序数组排序
-入参：  str 未排序的数组
+入参：  arr 未排序的数组
         left 数组左下标
         riht 数组右下表
-出参：  str 有序数组
+出参：  arr 有序数组
 返回值：无
 */
-void quicksort(int *str,int left,int right){
+void quicksort(int *arr,int left,int right){
     int i = 0 ,j =0;
     int center;
     if(left>=right)
         return;
     else
     {
-        center = partion(str,left,right);
-        quicksort(str,left,center-1);
-        quicksort(str,center+1,right);
-    }    
-      
-    
-    
+        center = partion(arr,left,right);
+        quicksort(arr,left,center-1);
+        quicksort(arr,center+1,right);
+    }     
 }
+/*
+函数名：SelectionSort
+功能：使用冒泡法对长度为len的无序数组排序
+入参：  arr 未排序的数组
+        len 数组长度
+出参：  arr 有序数组
+返回值：无
+*/
+void SelectionSort(int arr[],int len){
+;
+}
+void test_swap(int arr[],int len){
+    struct  timeval start,end;
+    unsigned  long diff;
+    
+    gettimeofday(&start,NULL);
+    //冒泡排序
+    bubblesort(arr,len); 
+    gettimeofday(&end,NULL);
+    diff = 1000000 * (end.tv_sec-start.tv_sec)+ end.tv_usec-start.tv_usec; 
+//    printf("bubblesort time is %ld\n",diff);
+    printf("bubblesort time is %ld\n ",diff);  
+    printf("sort arr is :\n");    
+    print_arr(arr,len);
+        
+     //快速排序
+    gettimeofday(&start,NULL);
+    quicksort(arr,0,len-1);
+    gettimeofday(&end,NULL);
+    diff = 1000000 * (end.tv_sec-start.tv_sec)+ end.tv_usec-start.tv_usec; 
+    printf("quicksort time is %ld\n",diff);
+    printf("quicksort time is %ld\n",diff);      
+    printf("quicksort arr is :\n");    
+//    print_arr(arr,len);        
+}
+
 int main(int argc,char *argv[]){
     
     int len = 10;
-    int str[1000];
-    time_t stime;
+    int arr[100000];
+
+  
     if( argc == 2){
         len = atoi(argv[1]);
-        printf("str len is set %d\n",len);
+        printf("arr len is set %d\n",len);
     }
    //初始化数组 
-    initstr(str,len);   
-   
- printf("init rand str is :\n");
-    print_str(str,len);
+    initarr(arr,len);   
+    printf("init rand arr is :\n");
+    print_arr(arr,len);
+    
+    test_swap(arr,len);
+ 
 
-/*
-//    stime = time(NULL);
-    //冒泡排序
-    bubblesort(str,len); 
- //   printf("sort time is %d\n",time(NULL)-stime);     
-    printf("sort str is :\n");    
-    print_str(str,len);
- */  
-     //快速排序
-//    stime = time(NULL);
-    quicksort(str,0,len-1);
- //   printf("quicksort time is %d\n",time(NULL)-stime);      
-    printf("quicksort str is :\n");    
-    print_str(str,len);  
-       
-     return 0; 
+
+  
+     
+    return 0; 
 }
